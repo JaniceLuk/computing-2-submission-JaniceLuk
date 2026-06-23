e0c1f1e76112247d6b14eabaca082a16b3906b63
JaniceLuk <Janice.luk24@imperial.ac.uk>
2026-06-23 02:03:16 +0100

    bug fixing

--- web-app/pawn-law.js
+++ web-app/pawn-law.js
@@ -1,37 +1,77 @@
+"use strict";
+
+// push all pawn elements inside this array
 const arrayofPawn = [];
 
+// push my black pawn elements
 for (const i of filesNameArray) {
     arrayofPawn.push(document.getElementById(i + 2));
 }
+
+// push my white pawn elements
 for (const i of filesNameArray) {
     arrayofPawn.push(document.getElementById(i + 7));
 }
 
+// add eventlistner on all pawn elements(arrayOfPawn)
 for(const i of arrayofPawn){
     i.addEventListener("click", () => {
+        // print clicked pawn element
+        // console.log(i);
+
+        // store clicked element
+        const clickedElement = i;
+
+        // find id of clicked element
         const currentId = i.getAttribute("id");
+
+        // print id of clicked pawn element
         console.log(currentId);
+
+        // get the number from id
         let change = parseInt(currentId[1]);
-        const stepsSquare = [];
+        console.log(change);
+
+        // array of elements that we want to add circles
+        const addCircleToElements = [];
+
+        // push elements in array (to which we want to add circles)
         for (let i = 0; i < 2; i++) {
             change++
-            stepsSquare.push(
+            addCircleToElements.push(
                 document.getElementById(currentId[0] + change));
         }
-        console.log(stepsSquare);
+        // add circle to each elements of passed array
+        highlightCircle(addCircleToElements);
     });
 }
 
-// function to add highlighting
+// array to track circles
+const trackCircleArray = [];
 
-const highlightCircle = function ([first, second]) {
-    let circle = document.createElement("div");
-    let circle2 = document.createElement("div");
-    circle.setAttribute("class", "circle");
-    circle2.setAttribute("class", "circle");
-    first.appendChild(circle);
-    second.appendChild(circle2);
-    console.log(first, second);
-    first.classList.add("flex");
-    second.classList.add("flex");
+// add circle to each element of passed array
+const highlightCircle = function (attachCircles) {
+
+    if(trackCircleArray.length != 0){
+        undoCircle(trackCircleArray);
+
+    }
+
+    // create child element
+    const child = `<div class="circle"></div>`
+
+    attachCircles.forEach((el) => {
+        el.innerHTML = child;
+        el.classlist.add("flex");
+        trackCircleArray.push(el);
+    });
+    console.log(trackCircleArray);
+};
+
+// remove circles from elements
+const undoCircle = function(circlesArray){
+    circlesArray.forEach((cur)=>{
+        cur.innerHTML = "";
+        cur.classList.remove("circle");
+    });
 };
